b64d9f596ae7e93d7b39501d5180620587797f94
JayDrojas <damianxrojas@gmail.com>
2022-05-16 21:46:56 -0700

    styled reply form and tweet details page

--- react-app/src/components/Replies/CreateReplyForm.js
+++ react-app/src/components/Replies/CreateReplyForm.js
@@ -1,22 +1,21 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import { create_reply } from "../../store/replies";
 
-const CreateReplyForm = ({tweet}) => {
+const CreateReplyForm = ({ tweet }) => {
   const user = useSelector(state => state.session.user)
   const dispatch = useDispatch()
-  const [content, setContent] = useState();
-  const [errors, setErrors] = useState();
+  const [content, setContent] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     const tweetErrors = []
-    if(content.length <= 0) tweetErrors.push('Chirp can\'t be empty.')
-    if(content.length > 255) tweetErrors.push('Chirp can\'t be more than 255 characters long.')
+    if (content.length <= 0) tweetErrors.push('Chirp can\'t be empty.')
+    if (content.length > 255) tweetErrors.push('Chirp can\'t be more than 255 characters long.')
 
-    if(tweetErrors.length) return setErrors(tweetErrors)
+    if (tweetErrors.length) return setErrors(tweetErrors)
 
     const newReply = {
       content,
@@ -29,19 +28,46 @@ const CreateReplyForm = ({tweet}) => {
     setErrors([])
   }
 
+  function setNewSize(e) {
+    if (content.length >= 256) {
+      e.target.style.color = "red";
+    } else {
+      e.target.style.color = "#fff";
+    }
+    e.target.style.height = "10px";
+    e.target.style.height = e.target.scrollHeight + "px";
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="content">Create Reply</label>
-      <textarea
-        name="content"
-        type="text"
-        placeholder="Chirp your reply"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        required={true}
-      />
-      <button type="submit">Reply</button>
-    </form>
+    <div className="create-tweet-form">
+      <i class="fa-solid fa-user" style={{ fontSize: "35px" }}></i>
+      <form onSubmit={handleSubmit}>
+        <div className="create-tweet-textarea">
+        <div>
+          {errors.length > 0 && (
+            errors.map(error => (
+              <p>{error}</p>
+              ))
+              )}
+              </div>
+              <p>Replying to {tweet?.user.username}</p>
+          <textarea
+            name="content"
+            id="myTextarea"
+            onKeyUp={setNewSize}
+            type="text"
+            placeholder="Chirp your reply"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required={true}
+          />
+        </div>
+        <div className="crt-chrp-div">
+          {content.length > 255 && (<span id="span-character-error">{255 - content?.length}</span>)}
+          <button className="create-chirp-bttn-form" type="submit">Reply</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
