e13af85ef946d098a8d9fbf6cd3e9d6a34ea6a44
JayDrojas <damianxrojas@gmail.com>
2022-05-19 18:24:39 -0700

    fixed some warnings

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
 import Home from './components/Home'
 import SplashPage from './components/SplashPage';
-import Tweet from './components/Tweets/Tweet';
 import TweetsDetail from './components/Home/tweets-detail';
 
 function App() {
